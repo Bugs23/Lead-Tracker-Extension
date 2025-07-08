ded90bcaeb4c4776a0229a51b1917b0f2487a91b
Raymond <raymondurrutia2@yahoo.com>
2025-07-08 05:52:37 -0400

    First commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+// Create an array to store leads
+let myLeads = [];
+
+// Add an event listener to the button so when the button is clicked,
+// the value from the input field is added to the array and the list is re-rendered
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  renderLeads();
+});
+
+//
+function renderLeads() {
+  // Create a string to hold the list items
+  let listItems = "";
+
+  // Loop through the myLeads array and create a list item for each lead
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+
+  // Add the list items to the ul element
+  ulEl.innerHTML = listItems;
+}
