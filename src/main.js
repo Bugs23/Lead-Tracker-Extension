558ef500ebfbec1501542dfad089a328310edf48
Raymond <raymondurrutia2@yahoo.com>
2025-07-11 05:01:05 -0400

    Create tab button to save url of current tab

--- src/main.js
+++ src/main.js
@@ -2,44 +2,55 @@ const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const tabBtn = document.getElementById("tab-btn");
 
-let myLeads = []; // Create an array to store leads
+let myLeads = [];
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")); // Retrieve leads from local storage and parse them into an array
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-// Check if there are leads stored in local storage
+// Check if there are leads stored in local storage and assign them to myLeads
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage; // If there are leads, assign them to myLeads
-  renderLeads(); // Render the leads if there are any in local storage
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-// Add click event to input button so when it's clicked, the value from the input is added to the array and the list is re-rendered
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+// Add click event to input button so when it's clicked,
+// the value from the input is added to the array and the list is re-rendered
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value); // Add the value from the input field to the myLeads array
-  inputEl.value = ""; // Clear the input field after adding the lead
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // Save the leads to local storage
-  renderLeads();
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 
   console.log(localStorage.getItem("myLeads"));
 });
 
-// Add dblclick event to delete button so when it's clicked, the leads are cleared from local storage and the list is re-rendered
+// Add dblclick event to delete button so when it's clicked,
+// the leads are cleared from local storage and the list is re-rendered
 deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear(); // Clear the leads from local storage
-  myLeads = []; // Reset the myLeads to an empty array
-  renderLeads();
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
 });
 
-// Render the leads in the ul element
-function renderLeads() {
-  let listItems = ""; // Create a string to hold the list items
+// Render the array that's passed in the ul element
+function render(leads) {
+  let listItems = "";
 
-  // Loop through the myLeads array and create a list item for each lead
-  for (let i = 0; i < myLeads.length; i++) {
+  // Create a list item for each lead
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+        <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
       </li>`;
   }
 
-  ulEl.innerHTML = listItems; // Add the list items to the ul element
+  ulEl.innerHTML = listItems;
 }
